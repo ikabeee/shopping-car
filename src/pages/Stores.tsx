d833401c6e209bde7863f268358e324fda74a0f2
ikabeee <carlglz30@gmail,com>
2024-10-20 21:20:02 -0500

    Add page store

---
+++ src/pages/Stores.tsx
@@ -0,0 +1,16 @@
+import MapStore from "../components/MapStore"
+
+
+const Stores = () => {
+    return(
+        <>
+
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 map-container">
+            <MapStore />
+        </div>
+
+        </>
+    )
+}
+
+export default Stores
