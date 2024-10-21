33c981b13ac2a1ea3eaddc5169eba5bfcf56cee1
ikabeee <carlglz30@gmail.com>
2024-10-21 17:45:50 -0500

    Add next ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 
 //Importar los estilos de bootstrap7
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header'
+import Header from './components/layout/Header'
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import About from './pages/About';
 import Stores from './pages/Stores';
-
-
+import Footer from './components/layout/Footer';
 
 
 function App() {
 
 
   return (
+
     <>
+
       <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
@@ -24,9 +24,9 @@ function App() {
         <Route path='/about-us' element={<About/>} />
         <Route path='/stores' element={<Stores/>} />
        </Routes>
-        
-   
-    </>
+       <Footer/>
+
+       </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { NextUIProvider } from '@nextui-org/react'
 import App from './App.tsx'
+import React from 'react'
+import './index.css'
 
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <NextUIProvider>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </NextUIProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 )
